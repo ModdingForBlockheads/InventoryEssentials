e82aafe519a48893c414cd027dfd20b102fd0934
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:43:05 +0100

    feat: Update for Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/inventoryessentials/ForgeInventoryEssentials.java
+++ forge/src/main/java/net/blay09/mods/inventoryessentials/ForgeInventoryEssentials.java
@@ -1,21 +1,21 @@
 package net.blay09.mods.inventoryessentials;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.blay09.mods.inventoryessentials.client.InventoryEssentialsClient;
 import net.minecraft.world.inventory.Slot;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.items.SlotItemHandler;
 
 @Mod(InventoryEssentials.MOD_ID)
 public class ForgeInventoryEssentials {
 
-    public ForgeInventoryEssentials() {
+    public ForgeInventoryEssentials(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
         PlatformBindings.INSTANCE = new PlatformBindings() {
             @Override
             public boolean isSameInventory(Slot targetSlot, Slot slot) {
@@ -27,10 +27,12 @@ public class ForgeInventoryEssentials {
             }
         };
 
-        Balm.initialize(InventoryEssentials.MOD_ID, EmptyLoadContext.INSTANCE, InventoryEssentials::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(InventoryEssentials.MOD_ID, EmptyLoadContext.INSTANCE, InventoryEssentialsClient::initialize));
+        Balm.initialize(InventoryEssentials.MOD_ID, loadContext, InventoryEssentials::initialize);
+        if (FMLEnvironment.dist.isClient()) {
+            BalmClient.initialize(InventoryEssentials.MOD_ID, loadContext, InventoryEssentialsClient::initialize);
+        }
 
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
+        context.registerDisplayTest(IExtensionPoint.DisplayTest.IGNORE_ALL_VERSION);
     }
 
 }
